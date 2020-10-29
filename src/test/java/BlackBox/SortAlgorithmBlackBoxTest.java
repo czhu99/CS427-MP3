6dba710db194ce4e3863c04acb110757ea5c2dad
Zhu <cjzhu2@fa20-cs427-038.cs.illinois.edu>
2020-10-29 02:12:42 -0500

    First commit

---
+++ src/test/java/BlackBox/SortAlgorithmBlackBoxTest.java
@@ -0,0 +1,17 @@
+package BlackBox;
+
+import BlackBox.Setups.SortSetup;
+import org.junit.Test;
+
+public class SortAlgorithmBlackBoxTest extends SortSetup {
+
+    @Test
+    public void dummyTest(){
+        //@TODO: Delete/modify me
+        int[] input = new int[]{2,1};
+        int[] expectedOutput = new int[]{1,2};
+        sortAlgorithmPUT.run(input,expectedOutput);
+    }
+
+    //@TODO: Create more tests
+}
